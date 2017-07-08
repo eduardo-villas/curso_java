b6536153b272f7b042a5fe9e94b8a43136c6dc45
Eduardo Villas Boas dos Santos <eduardovillasboassantos@gmail.com>
2017-07-08 13:32:54 -0300

    Classes e objetos exemplo

---
+++ ClassesEObjetos1/src/classeseobjetos1/ClassesEObjetos.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classeseobjetos1;
+
+import nossosbojetos.Carro;
+import static java.lang.System.out;
+/**
+ *
+ * @author eduardov
+ */
+public class ClassesEObjetos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //variável local ao metodo main
+        Carro miniCooper = new Carro();
+        
+        miniCooper.setMotor("MOTO BMW V6");
+        miniCooper.setMarca("BMW");
+        miniCooper.setModelo("Mini Cooper");
+        miniCooper.setAnoFabricacao(2016);
+        
+        Carro gol = new Carro();
+        
+        gol.setMotor("VOLKSWAGEN 1.6");
+        gol.setMarca("VOLKSWAGEN");
+        gol.setModelo("Gol");
+        gol.setAnoFabricacao(2015);
+        
+        //temos dois objetos que representam um carro
+        //um carro tem muito mais atributos, porém é um exemplo.
+        //este metodo mostra os atributos do carro.
+        //o conjunto de atributos de um carro em um dado momento é
+        //chamado estado do objeto.
+        mostraEstado(miniCooper);
+        mostraEstado(gol);
+        
+    }
+
+    private static void mostraEstado(Carro carro) {
+        String modelo = carro.getModelo();
+        String marca = carro.getMarca();
+        String motor = carro.getMotor();
+        int anoFabricadao = carro.getAnoFabricacao();
+        out.printf("Modelo: %s \n"
+                + "Marca: %s\n"
+                + "Motor: %s\n"
+                + "Ano de fabricação:%d\n",
+                modelo, marca, motor, anoFabricadao);
+        out.println();        
+    }
+    
+}
---
+++ ClassesEObjetos1/src/nossosbojetos/Carro.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nossosbojetos;
+
+/**
+ *
+ * @author eduardov
+ */
+public class Carro {
+
+    private String marca;
+    private String modelo;
+    private String motor;
+    private int anoFabricacao;
+    
+    //construtor sem argumentos
+    //o compilador já oferece um construtor sem argumentos.
+    //tente deletar o construtor e compilar sem ele.
+    public Carro() {
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMotor() {
+        return motor;
+    }
+
+    public void setMotor(String motor) {
+        this.motor = motor;
+    }
+
+    public int getAnoFabricacao() {
+        return anoFabricacao;
+    }
+
+    public void setAnoFabricacao(int anoFabricacao) {
+        this.anoFabricacao = anoFabricacao;
+    }
+    
+}
