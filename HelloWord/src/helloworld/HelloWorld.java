4eb9d6e9d3a8bc00d0a12af2d7add64688da76c7
Eduardo Villas Boas dos Santos <eduardovillasboassantos@gmail.com>
2017-07-07 23:02:34 -0300

    commit inicial

---
+++ HelloWord/src/helloworld/HelloWorld.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//pacote da classe
+package helloworld;
+
+/**
+ *
+ * @author eduardov
+ */
+//classe publica HelloWorld
+// classes tem um 
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    //metod statico invocado pela jvm. Porque é necessário?
+    //recebe os argumentos pela linha de comando.
+    //metodos e cla
+    // coloque o seguinte codigo no metodo main
+    // System.out.println(args.length);
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+    
+}
